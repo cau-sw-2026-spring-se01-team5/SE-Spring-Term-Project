b04dd2cd8fdf093a87264ef9f223b8a0d383cc9d
joshua <joshuab1b2823@gmail.com>
2026-05-11 14:46:00 +0900

    javafx 모듈 추가

--- core/src/main/java/controller/TestController.java
+++ core/src/main/java/controller/TestController.java
@@ -2,6 +2,6 @@ package controller;
 
 public class TestController {
     public void hello() {
-        System.out.println("Hello world");
+        System.out.println("Hello world from TestController");
     }
 }
---
+++ javafx/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import controller.TestController;
+
+public class Main {
+    public static void main(String[] args) {
+        TestController controller = new TestController();
+        controller.hello();
+    }
+}
