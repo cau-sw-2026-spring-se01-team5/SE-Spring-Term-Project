58201f9970adbe3571d24f467d21dddaa1bc9c94
joshua <joshuab1b2823@gmail.com>
2026-05-21 17:03:04 +0900

    core: ProjectImplTest 추가

--- core/src/main/java/controller/project/v2/ProjectImpl.java
+++ core/src/main/java/controller/project/v2/ProjectImpl.java
@@ -1,5 +1,7 @@
 package controller.project.v2;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import project.dto.createProject.v1.CreateProjectInput;
 import project.dto.createProject.v1.CreateProjectOutput;
 import project.dto.deleteProject.v1.DeleteProjectInput;
@@ -9,8 +11,16 @@ import project.dto.getProjectList.v1.GetProjectListOutput;
 import project.dto.updateProjectInfo.v1.UpdateProjectInfoInput;
 import project.dto.updateProjectInfo.v1.UpdateProjectInfoOutput;
 import project.v2.Project;
+import repository.ProjectRepository;
+import repository.UserRepository;
 
+@RequiredArgsConstructor
 public class ProjectImpl implements Project {
+    @NonNull
+    private UserRepository userRepository;
+    @NonNull
+    private ProjectRepository projectRepository;
+
     @Override
     public CreateProjectOutput createProject(project.dto.createProject.v2.CreateProjectInput input) {
         return null;
--- core/src/test/java/controller/project/v2/ProjectImplTest.java
+++ core/src/test/java/controller/project/v2/ProjectImplTest.java
@@ -1,17 +1,59 @@
 package controller.project.v2;
 
+import domain.Project;
+import domain.User;
+import enums.user.v1.UserRole;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import project.dto.createProject.v1.CreateProjectOutput;
+import project.dto.createProject.v2.CreateProjectInput;
+import repository.ProjectRepository;
+import repository.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class ProjectImplTest {
+    ProjectImpl projectImpl;
+    User admin, nonAdmin;
+    Project newProject;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        admin = new User("admin", "1234", UserRole.ADMIN);
+        admin.setId(1);
+        nonAdmin = new User("non-admin", "1234", UserRole.DEV);
+        nonAdmin.setId(2);
+        newProject = new Project("Test Project");
+        newProject.setId(10);
+
+        UserRepository userRepository = mock(UserRepository.class);
+        ProjectRepository projectRepository = mock(ProjectRepository.class);
+
+        when(userRepository.load(admin.getId())).thenReturn(admin);
+        when(userRepository.load(nonAdmin.getId())).thenReturn(nonAdmin);
+        when(projectRepository.save(any())).thenReturn(newProject.getId());
+
+        projectImpl = new ProjectImpl(userRepository, projectRepository);
+    }
 
     @Test
     void createProject() {
+        CreateProjectOutput output = projectImpl.createProject(
+                new CreateProjectInput(admin.getId(), newProject.getName())
+        );
+
+        assertEquals(newProject.getId(), output.projectId());
     }
 
     @Test
     void nonAdmin() {
+        CreateProjectOutput output = projectImpl.createProject(
+                new CreateProjectInput(nonAdmin.getId(), newProject.getName())
+        );
 
+        assertEquals(false, output.success());
     }
 }
